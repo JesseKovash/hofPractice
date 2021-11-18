bc5b0dc58f16b5d037b6530f5fb28c14d49d00cd
Jesse Kovash <jessekovash@yahoo.com>
2021-11-17 19:14:15 -0700

    progress 1

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // This repo is optional extra practice to use the underscore functions.
-// Here we'll be writing new functions, but these functions will use 
-// the underscore functions within them. 
+// Here we'll be writing new functions, but these functions will use
+// the underscore functions within them.
 
 /*
  *
@@ -22,7 +22,13 @@ var moreFruits = function(fruits) {
 // use _.each to traverse the number array and determine
 // which are multiples of five.
 var multiplesOfFive = function(numbers) {
-
+  var multFiveCount = 0;
+  _.each(numbers, function(number) {
+    if (number % 5 === 0) {
+      multFiveCount ++;
+    }
+  });
+  return multFiveCount;
 };
 
 /*
@@ -33,18 +39,53 @@ var multiplesOfFive = function(numbers) {
 
 // use _.filter to return the fruits array with only the desired fruit.
 var onlyOneFruit = function(fruits, targetFruit) {
-
+  var filtered;
+  var correctFruit = _.filter(fruits, function (fruit, index) {
+    if (fruit === targetFruit) {
+      filtered = fruits.slice(index, index + 1);
+    }
+  });
+  return filtered;
 };
+
 
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function(fruits, letter) {
-
+  var startsFiltered;
+  _.filter(fruits, function(fruit, index) {
+    if (fruit[0] === letter) {
+      if (startsFiltered === undefined) {
+        startsFiltered = fruits.slice(index, index + 1);
+      } else {
+        startsFiltered.push(fruit);
+      }
+    }
+  });
+  if (startsFiltered === undefined) {
+    return (fruits.slice(fruits.length + 1));
+  } else {
+    return startsFiltered;
+  }
 };
 
 // return a filtered array containing only cookie-type desserts.
 var cookiesOnly = function(desserts) {
-
+  var cookieFiltered;
+  _.filter(desserts, function(dessert, index) {
+    if (dessert.type === 'cookie') {
+      if (cookieFiltered === undefined) {
+        cookieFiltered = desserts.slice(index, index + 1);
+      } else {
+        cookieFiltered.push(dessert);
+      }
+    }
+  });
+  if (cookieFiltered === undefined) {
+    return (desserts.slice(desserts.length + 1));
+  } else {
+    return cookieFiltered;
+  }
 };
 
 /*
@@ -55,27 +96,62 @@ var cookiesOnly = function(desserts) {
 
 // return the total price of all products.
 var sumTotal = function(products) {
-  
+  var totaled = _.reduce(products, function (start, product) {
+    var priced = parseFloat(product.price.substring(1));
+    return start + priced;
+  }, 0);
+  return totaled;
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function(desserts) {
+  var sortedDesserts = _.reduce(desserts, function (memo, eachDessert) {
+    var dessertType = eachDessert.type;
 
+    if (dessertType) {
+      if (memo[dessertType] === undefined) {
+        memo[dessertType] = 1;
+      } else {
+        memo[dessertType] = memo[dessertType] + 1;
+      }
+    }
+    return memo;
+
+  }, {});
+  return sortedDesserts;
 };
 
 // given an array of movie data objects,return an array containing
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function(movies) {
-  
+  var nineties = _.reduce(movies, function (memo, eachMovie) {
+    var year = eachMovie.releaseYear;
+    if (year >= 1990 && year <= 2000) {
+      memo.push(eachMovie.title);
+      return memo;
+    } else {
+      return memo;
+    }
+  }, []);
+  return nineties;
 };
 
 // return an boolean stating if there exists a movie with a shorter
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
 var movieNight = function(movies, timeLimit) {
-
+  var shortMovies = _.reduce (movies, function(memo, movie) {
+    var movieTime = movie.runtime;
+    if (movieTime) {
+      if (movieTime < timeLimit) {
+        memo = true;
+      }
+    }
+    return memo;
+  }, false);
+  return shortMovies;
 };
 
 /*
@@ -102,7 +178,7 @@ var glutenFree = function(desserts) {
 //
 // having trouble with decimals? check out this article:
 // http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
-// 
+//
 /*
 
  example output:
@@ -111,7 +187,7 @@ var glutenFree = function(desserts) {
     {
       id: 1,
       product: 'Olive Oil',
-      price: '$12.1', 
+      price: '$12.1',
       salePrice: '$9.61'
     }
   ];
